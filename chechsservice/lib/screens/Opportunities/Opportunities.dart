0f3bdf5e3e9f9d80a6fd1b4fa43de81be996a55f
Wahab M. Alshahin <wahab.alshahin@nasa.gov>
2020-02-25 17:38:18 -0600

    Added new code for opportunities

--- chechsservice/lib/screens/Opportunities/Opportunities.dart
+++ chechsservice/lib/screens/Opportunities/Opportunities.dart
@@ -1,18 +1,120 @@
 // Import: Flutter
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+// Import Third Party
+import 'package:provider/provider.dart';
+// Import chechsservice
+import 'package:chechsservice/state/AppState.dart';
 
 // ======================================
 // WIDGET: Opportunities
 // ======================================
 
-class Opportunities extends StatelessWidget {
+class Opportunities extends StatefulWidget {
+  @override
+  _OpportunitiesState createState() => _OpportunitiesState();
+}
+
+class _OpportunitiesState extends State<Opportunities> {
   @override
   Widget build(BuildContext context) {
+    AppState state = Provider.of<AppState>(context);
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+        child: ListView(
+          children: <Widget>[
+            for (var item in state.opportunities.keys)
+              OpportunityCard(
+                opportunityIndex: item,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ======================================
+// WIDGET: OpportunityCard
+// ======================================
+
+class OpportunityCard extends StatefulWidget {
+  // Define out inputs
+  final int opportunityIndex;
+  OpportunityCard({
+    Key key,
+    @required this.opportunityIndex,
+  }) : super(key: key);
+
+  @override
+  _OpportunityCardState createState() => _OpportunityCardState();
+}
+
+class _OpportunityCardState extends State<OpportunityCard> {
+  @override
+  Widget build(BuildContext context) {
+    AppState state = Provider.of<AppState>(context);
+
+    // Extract the opportunity
+    Map opt = state.opportunities[widget.opportunityIndex];
+
+    String startTime = DateFormat.yMMMMd('en_US').add_jm().format(opt['start']);
+    String endTime = DateFormat.yMMMMd('en_US').add_jm().format(opt['end']);
+
     return Container(
-      child: RaisedButton(
-        child: Text("Add"),
-        onPressed:(){},
-        color: Colors.orange,
+      margin: EdgeInsets.all(5),
+      color: Colors.blue,
+      child: ExpansionTile(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              opt['location'],
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              startTime,
+              style: TextStyle(
+                color: Colors.grey.shade100,
+                fontSize: 12,
+                height: 1.5,
+              ),
+            ),
+            Text(
+              endTime,
+              style: TextStyle(
+                color: Colors.grey.shade100,
+                fontSize: 12,
+                height: 1.5,
+              ),
+            ),
+          ],
+        ),
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(15, 10, 10, 5),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(opt['organization']),
+                Text(opt['contactPhone']),
+                Text(opt['contactEmail']),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- chechsservice/lib/state/AppState.dart
+++ chechsservice/lib/state/AppState.dart
@@ -1,6 +1,79 @@
 // Import: Flutter
 import 'package:flutter/material.dart';
 
+// ======================================
+// CLASS: AppState
+// ======================================
+
 class AppState with ChangeNotifier {
   bool nhsHoursCheck = true;
+
+  // Define all the global variables
+  String name = '';
+  String username = '';
+  String password = '';
+  Map hoursHistory = {};
+  Map opportunities = {};
+
+  // Class Constructor
+  AppState() {
+    setDefault();
+  }
+
+  // ======================================
+  // METHOD: setDefault
+  // ======================================
+
+  void setDefault() {
+    name = 'Luke Tedesco';
+    username = 'username';
+    password = 'password';
+
+    // Add some sample hours history
+    hoursHistory[0] = {};
+    hoursHistory[0]['start'] = DateTime(2020, 1, 1, 13, 0);
+    hoursHistory[0]['end'] = DateTime(2020, 1, 1, 19, 30);
+    hoursHistory[0]['name'] = 'My first hours history input';
+    hoursHistory[0]['organization'] = 'My first organization';
+    hoursHistory[0]['nhs'] = false;
+    hoursHistory[1] = {};
+    hoursHistory[1]['start'] = DateTime(2020, 2, 1, 1, 0);
+    hoursHistory[1]['end'] = DateTime(2020, 2, 1, 2, 30);
+    hoursHistory[1]['name'] = 'My second hours history input';
+    hoursHistory[1]['organization'] = 'My second organization';
+    hoursHistory[1]['nhs'] = true;
+
+    // Add some sample opportunities
+    opportunities[0] = {};
+    opportunities[0]['start'] = DateTime(2020, 1, 1, 13, 0);
+    opportunities[0]['end'] = DateTime(2020, 1, 1, 19, 0);
+    opportunities[0]['location'] = 'Oppotunity 1 location';
+    opportunities[0]['organization'] = 'Oppotunity 1 organization';
+    opportunities[0]['contactPhone'] = 'xxx-xxx-xxxx';
+    opportunities[0]['contactEmail'] = 'oppotunity1@gmail.com';
+    opportunities[1] = {};
+    opportunities[1]['start'] = DateTime(2020, 3, 1, 1, 0);
+    opportunities[1]['end'] = DateTime(2020, 3, 1, 4, 0);
+    opportunities[1]['location'] = 'Oppotunity 2 location';
+    opportunities[1]['organization'] = 'Oppotunity 2 organization';
+    opportunities[1]['contactPhone'] = 'xxx-xxx-xxxx';
+    opportunities[1]['contactEmail'] = 'oppotunity2@gmail.com';
+    opportunities[2] = {};
+    opportunities[2]['start'] = DateTime(2020, 3, 1, 1, 0);
+    opportunities[2]['end'] = DateTime(2020, 3, 1, 4, 0);
+    opportunities[2]['location'] = 'Oppotunity 3 location';
+    opportunities[2]['organization'] = 'Oppotunity 3 organization';
+    opportunities[2]['contactPhone'] = 'xxx-xxx-xxxx';
+    opportunities[2]['contactEmail'] = 'oppotunity2@gmail.com';
+
+    opportunities[3] = opportunities[2];
+    opportunities[4] = opportunities[2];
+    opportunities[5] = opportunities[2];
+    opportunities[6] = opportunities[2];
+    opportunities[7] = opportunities[2];
+    opportunities[8] = opportunities[2];
+    opportunities[9] = opportunities[2];
+
+
+  }
 }
